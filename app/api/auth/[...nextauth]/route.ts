5e00d33df9b6dc0967d29a653c6ac76f066b10a6
Luis Angel Sotelo Hernández <43999222+LuisSotelo@users.noreply.github.com>
2026-03-11 16:29:24 -0700

    Mods can enter to the panel.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,14 @@
 import NextAuth from "next-auth";
 import TwitchProvider from "next-auth/providers/twitch";
 
+
+interface TwitchProfile {
+  sub: string;
+  preferred_username: string;
+  // Usamos el nombre exacto que te arrojó el error
+  "https://api.twitch.tv/helix/users/extensions/moderator"?: boolean;
+}
+
 const handler = NextAuth({
   providers: [
     TwitchProvider({
@@ -11,7 +19,16 @@ const handler = NextAuth({
   // Eliminamos el objeto "pages" para usar las default de NextAuth
   callbacks: {
     async signIn({ profile }) {
-      return profile?.sub === process.env.AUTHORIZED_TWITCH_ID;
+      // Forzamos a TS a tratar el profile como nuestro TwitchProfile
+      const p = profile as unknown as TwitchProfile;
+
+      // Solo tú o tus moderadores de canal pueden entrar
+      const isOwner = p?.sub === process.env.MY_TWITCH_ID;
+      const isMod = p?.['https://api.twitch.tv/helix/users/extensions/moderator'] === true; 
+
+      console.log(`Intento de login: ${p.preferred_username} | Owner: ${isOwner} | Mod: ${isMod}`);
+      
+      return isOwner || isMod;
     },
   },
 });
